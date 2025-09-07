d755f37bb124978d5bf5975cd331830fd18e288b
apeksha172004 <apugaikwad1704@gmail.com>
2025-09-07 18:08:34 +0530

    in upload

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -1,4 +1,12 @@
 package HospitalManagementSystem;
 
 public class HospitalManagementSystem {
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/hospital";
+    private static final String username = "root";
+    private static final String password = "#Apek1712";
+
+    public static void main(String[] args) {
+
+    }
+
 }
